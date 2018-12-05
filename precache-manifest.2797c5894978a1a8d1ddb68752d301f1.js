472cd0439c086dc1d8b45446bfff713e793a24a3
Darshil Chapadia <darshil.chapadia@cowrks.com>
2018-12-06 01:18:39 +0530

    Updates

---
+++ precache-manifest.2797c5894978a1a8d1ddb68752d301f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/live-stock/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fc63e9d1d47dc8bfd751",
+    "url": "/live-stock/static/js/runtime~main.fc63e9d1.js"
+  },
+  {
+    "revision": "1a1e4167b67bf2ed872b",
+    "url": "/live-stock/static/js/main.1a1e4167.chunk.js"
+  },
+  {
+    "revision": "b0e68e998781d68962d5",
+    "url": "/live-stock/static/js/1.b0e68e99.chunk.js"
+  },
+  {
+    "revision": "1a1e4167b67bf2ed872b",
+    "url": "/live-stock/static/css/main.b1e6aa7b.chunk.css"
+  },
+  {
+    "revision": "26ab5b7f24b0612d8d0d955992eb11f1",
+    "url": "/live-stock/index.html"
+  }
+];
